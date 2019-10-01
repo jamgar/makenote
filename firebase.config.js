f92b7c3bbf6c49f16d1821f4f5287970801ccb4d
github username <github email address>
2019-10-01 10:14:08 -0500

    -Add firebase -Add Auth and Tasks views -Add Auth and Tasks routes -Add auth check to routes

---
+++ firebase.config.js
@@ -0,0 +1,35 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+// firebase init
+const config = {
+  apiKey: "AIzaSyDjo1rNl_nUpCvahSf043_H7tgQ-A3WDRQ",
+  authDomain: "vuegram-43372.firebaseapp.com",
+  databaseURL: "https://vuegram-43372.firebaseio.com",
+  projectId: "vuegram-43372",
+  storageBucket: "vuegram-43372.appspot.com",
+  messagingSenderId: "984018126273",
+  appId: "1:984018126273:web:5eb96c59aff9de1e1fdb40",
+  measurementId: "G-8DF6BE6E59"
+};
+
+firebase.initializeApp(config);
+
+// firebase utils
+const db = firebase.firestore();
+const auth = firebase.auth();
+const currentUser = auth.currentUser;
+
+// firebase collections
+const userCollection = db.collection("users");
+const notesCollection = db.collection("notes");
+const tasksCollection = db.collection("tasks");
+
+export {
+  db,
+  auth,
+  currentUser,
+  userCollection,
+  notesCollection,
+  tasksCollection
+};
--- src/router.js
+++ src/router.js
@@ -1,26 +1,47 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Vue from 'vue'
+import Router from 'vue-router'
+import firebase from 'firebase'
 
-Vue.use(Router);
+import Auth from '@/views/Auth.vue'
+import Home from '@/views/Home.vue'
+import Tasks from '@/views/Tasks.vue'
+
+Vue.use(Router)
 
 export default new Router({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/",
-      name: "home",
+      path: '/',
+      name: 'home',
       component: Home
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: '/auth',
+      name: 'auth',
+      component: Auth
+    },
+    {
+      path: '/tasks',
+      name: 'tasks',
+      component: Tasks,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
-});
+})
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(x => x.meta.requiresAuth)
+  const currentUser = firebase.auth().currentUser
+
+  if (requireAuth && !currentUser) {
+    next('/auth')
+  } else if (requiresAuth && currentUser) {
+    next()
+  } else {
+    next()
+  }
+})
