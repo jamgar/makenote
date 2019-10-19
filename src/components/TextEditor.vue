baa1daaecfa222aaff73667bdad3533348a740f9
github username <github email address>
2019-10-19 09:02:24 -0500

    -Remove toast-ui vue editor -Add CkEditor for vue

---
+++ src/components/TextEditor.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <ckeditor :editor="editor" v-model="editorData"></ckeditor>
+  </div>
+</template>
+
+<script>
+import CKEditor from '@ckeditor/ckeditor5-vue'
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+
+export default {
+  name: 'TextEditor',
+  components: {
+    ckeditor: CKEditor.component
+  },
+  data() {
+    return {
+      editor: ClassicEditor,
+      editorData: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
     <h3>Dashboard</h3>
+    <TextEditor id="myeditor" />
   </div>
 </template>
 
 <script>
-export default {}
+import TextEditor from '@/components/TextEditor.vue'
+
+export default {
+  name: 'Dashboard',
+  components: {
+    TextEditor
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
