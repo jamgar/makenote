a5f81452029c4dfa42ebc3dd0702c9d47d0ebffc
github username <github email address>
2019-10-20 08:44:50 -0500

    -Add ckeditor from source -Add vue.config.js for ckeditor -Updated TextEditor for ckeditor

--- src/components/TextEditor.vue
+++ src/components/TextEditor.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
-    <ckeditor :editor="editor" v-model="editorData"></ckeditor>
+    <ckeditor
+      :editor="editor"
+      v-model="editorData"
+      :config="editorConfig"
+    ></ckeditor>
   </div>
 </template>
 
 <script>
 import CKEditor from '@ckeditor/ckeditor5-vue'
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor'
+import EssentialsPlugin from '@ckeditor/ckeditor5-essentials/src/essentials'
+import BoldPlugin from '@ckeditor/ckeditor5-basic-styles/src/bold'
+import ItalicPlugin from '@ckeditor/ckeditor5-basic-styles/src/italic'
+import LinkPlugin from '@ckeditor/ckeditor5-link/src/link'
+import ParagraphPlugin from '@ckeditor/ckeditor5-paragraph/src/paragraph'
 
 export default {
   name: 'TextEditor',
@@ -16,7 +25,19 @@ export default {
   data() {
     return {
       editor: ClassicEditor,
-      editorData: ''
+      editorData: '',
+      editorConfig: {
+        plugins: [
+          EssentialsPlugin,
+          BoldPlugin,
+          ItalicPlugin,
+          LinkPlugin,
+          ParagraphPlugin
+        ],
+        toolbar: {
+          items: ['bold', 'italic', 'link', 'undo', 'redo']
+        }
+      }
     }
   }
 }
